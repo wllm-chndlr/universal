1ea2ff056aed3efc831eb6a31f909a5ca6b18f4a
William Chandler <chndlr.wllm@gmail.com>
2018-12-08 22:07:38 -0600

    initial reconfig

--- src/components/header/nav/nav.js
+++ src/components/header/nav/nav.js
@@ -5,11 +5,14 @@ import { Container } from './nav.css';
 const Nav = () => (
   <Container>
     <ul>
+        <li>
+        <Link to="/">Home</Link>
+      </li>
       <li>
         <Link to="/about">About</Link>
       </li>
       <li>
-        <a href="https://github.com/fabe/gatsby-universal">GitHub</a>
+        <a href="https://github.com/wllm-chndlr">GitHub</a>
       </li>
     </ul>
   </Container>
