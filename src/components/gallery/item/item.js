9a15167e14e007d185d8bf84097702380d618c54
William Chandler <chndlr.wllm@gmail.com>
2019-01-03 20:35:32 -0600

    fixed link issue

--- src/components/gallery/item/item.js
+++ src/components/gallery/item/item.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
-import { Title, Copy, Link } from './item.css';
+import { Title, Copy } from './item.css';
 
 const Item = ({ title, copy, image, link }) => (
   <figure>
@@ -9,7 +9,7 @@ const Item = ({ title, copy, image, link }) => (
     <figcaption>
       <Title>{title}</Title>
       <Copy>{copy}</Copy>
-      <Link>{link}</Link>
+      <Title>{link}</Title>
     </figcaption>
   </figure>
 );
@@ -18,7 +18,7 @@ Item.propTypes = {
   title: PropTypes.string,
   copy: PropTypes.string,
   image: PropTypes.object.isRequired,
-  link: PropTypes.object.isRequired,
+  link: PropTypes.string,
 };
 
 export default Item;
